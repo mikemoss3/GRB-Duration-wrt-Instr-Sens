43c4c9e02ffd386a5008658b0c706b15e15ce293
Mike Moss <mikejmoss3@gmail.com>
2023-09-14 15:02:41 -0400

    Created unit tests for Bayesian block package

--- unit_tests/test_bayesian_blocks.py
+++ unit_tests/test_bayesian_blocks.py
@@ -1,6 +1,12 @@
-import numpy as np
+"""
+Author: Mike Moss
+Contact: mikejmoss3@gmail.com	
 
-from packages.package_bayesian_block import bayesian_blocks
+Defines the unit tests for the Bayesian block package 
+
+"""
+
+from packages.package_bayesian_block import bayesian_t_blocks
 from packages.class_GRB import GRB
 
 def run_test():
@@ -9,17 +15,20 @@ def run_test():
 	"""
 
 	# Light curve file with known source
-	light_curve_fn = "light_curve.txt"
+	light_curve_fn = "./unit_tests/test_files/grb_130831A_1chan_64ms.txt"
+	# light_curve_fn = "./unit_tests/test_files/grb_160314A_1chan_64ms.txt"
 
 	# Make GRB object to store light curve
 	grb = GRB(light_curve_fn=light_curve_fn)
 
 	# Run Bayesian block method
-	duration, fluence = bayesian_blocks(grb)
+	duration, fluence = bayesian_t_blocks(grb)
+
+	print(duration, fluence)
 
 	# Run tests
 	test_duration(duration)
-	test_fluence(fluence)
+	# test_fluence(fluence)
 
 	return 0;
 
@@ -27,13 +36,13 @@ def test_duration(duration):
 	"""
 	This method test if the correct duration was recovered 
 	"""
-	known_duration = 1 # second
+	known_duration = 8.732 # second
 
-	if (duration != known_duration):
+	if ((known_duration-0.5) < duration) and ( duration < (known_duration+0.5)):
+		return 0
+	else:
 		print("Duration found by Bayesian blocks does not match known source duration.")
 		return 1
-	else:
-		return 0
 
 def test_fluence(duration):
 	"""
@@ -46,7 +55,3 @@ def test_fluence(duration):
 		return 1
 	else:
 		return 0
-
-
-if __name__ == "__main__":
-	run_test()
