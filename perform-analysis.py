e603fa7ee50188227b4be6627e76c36d95cd8b12
Mike Moss <mikejmoss3@gmail.com>
2023-10-09 23:13:23 -0400

    Using code.

--- perform-analysis.py
+++ perform-analysis.py
@@ -8,17 +8,17 @@ from packages.package_bayesian_block import bayesian_t_blocks
 
 z = 1 # redshift 
 imx, imy = 0., 0. # Position on the detector plane
-ndets = 30000 # Number of enabled detectors
+ndets = 10000 # Number of enabled detectors
 
 # Make a GRB object
 template_grb = GRB()
 # Make light curve 
-template_grb.load_light_curve("data-files/sw00634795000b_1chan_1s.lc")
-template_grb.light_curve['TIME'] -= 448001706.176
-# template_grb.light_curve['TIME'] -= 544495556.416
+template_grb.load_light_curve("data-files/sw00330856000b_1chan_64ms.lc",rm_trigtime=True)
 template_grb.light_curve = template_grb.light_curve[np.argmax(-200<=template_grb.light_curve['TIME']):np.argmax(template_grb.light_curve['TIME']>=200)]
 
 template_grb.load_spectrum("data-files/test_spectrum.txt")
+
+template_grb.move_to_source_frame(z=z)
 
 # Make a Response Matrix object 
 resp_mat = ResponseMatrix()
@@ -36,7 +36,7 @@ print(synth_t90, t_start, t_end, synth_t90_flue)
 
 plt.plot(template_grb.light_curve['TIME'],template_grb.light_curve['RATE'])
 plt.plot(synth_grb.light_curve['TIME'],synth_grb.light_curve['RATE'])
-plt.axvspan(xmin=-1.2,xmax=13.5,color="C0",alpha=0.3)
+# plt.axvspan(xmin=-1.2,xmax=13.5,color="C0",alpha=0.3)
 plt.axvspan(xmin=t_start,xmax=t_end,color="C3",alpha=0.3)
 
 plt.xlim(-75,75)
