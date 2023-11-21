c02ad86958dd947ab088fdf9722ad662196c08a5
Michael Moss (NASA) <mjmoss@ndc.nasa.gov>
2023-11-21 15:22:36 -0500

    Custom for 060614 and 211211A

--- perform-analysis.py
+++ perform-analysis.py
@@ -16,45 +16,50 @@ from packages.class_SPECFUNC import PL, CPL
 from packages.package_analysis import many_simulations, make_param_list
 
 
-# Make a GRB object
-z = 0.1254 # redshift 
-template_grb = GRB(z=z)
-# Make light curve 
-template_grb.load_light_curve("data-files/template-light-curves/grb_060614_1chan_64ms.lc", rm_trigtime=True, det_area=0.16)
-# template_grb.load_light_curve("data-files/template-light-curves/grb_211211A_1chan_64ms.lc", rm_trigtime=True, det_area=0.16)
-template_grb.light_curve = template_grb.light_curve[np.argmax(-20 <= template_grb.light_curve['TIME']):np.argmax(template_grb.light_curve['TIME'] >= 160)]
-
+"""
+# GRB 060614 info
+z = 0.1254
+fn = "data-files/template-light-curves/grb_060614_1chan_64ms.lc"
 alpha = -2.33
 ep = 134.112
 norm = 9.47830E-03
+z_arr = np.array([0.1254,0.2,0.3,0.35,0.4])
+t90_obs = 109.104
+"""
+# GRB 211211 info
+z = 0.076
+fn = "data-files/template-light-curves/grb_211211A_1chan_64ms.lc"
+alpha = -1.442
+ep =  512.674
+norm = 0.135641E-01
+z_arr = np.array([0.076,0.1,0.2,0.3,0.4,0.5,0.55,0.6,0.65,0.7]) # 211211A
+t90_obs = 50.712
+
+
+# Make a GRB object
+template_grb = GRB(z=z)
+# Make light curve 
+template_grb.load_light_curve(fn, rm_trigtime=True, det_area=0.16)
+# template_grb.load_light_curve("data-files/template-light-curves/grb_211211A_1chan_64ms.lc", rm_trigtime=True, det_area=0.16)
+template_grb.light_curve = template_grb.light_curve[np.argmax(-20 <= template_grb.light_curve['TIME']):np.argmax(template_grb.light_curve['TIME'] >= 170)]
+
 template_grb.load_specfunc(CPL(alpha= alpha, ep=ep, norm=norm, enorm=50))
 
-
 # Simulate many observations 
-z_arr = np.array([0.1254,0.2,0.3,]) # 060614
-# z_arr = np.array([0.076,0.3,0.5,0.7,0.9,1.1,1.3,1.4,1.5,1.6,1.7,2]) # 211211A
-# z_arr = np.array([0.076,0.1,0.2,0.3]) # 211211A
-
 imx_arr = np.array([0.])
 imy_arr = np.array([0.])
-
 ndets_arr = np.array([30000])
 
 param_list = make_param_list(z_arr,imx_arr,imy_arr,ndets_arr)
 trials = 3
 
-
 sim_results, grbs = many_simulations(template_grb, param_list, trials, multiproc=False, ret_ave=True, keep_synth_grbs=True)
 
-
 template_grb.light_curve['RATE'] *= 0.16
-
-
 
 plots = PLOTS(sim_results)
 # plots.plot_light_curves(template_grb)
 # plots.det_plane_map()
-plots.dur_vs_param(obs_param="z",t_true=109.104,dur_frac=True)
-# plt.savefig("data-files/figs/2023-11-06/2023-11-06-grb-211211A-redshift-vs-dur-evo.png")
+plots.dur_vs_param(obs_param="z",t_true=t90_obs,dur_frac=True)
 plots.plot_light_curves(grbs=np.append(template_grb,grbs),labels=["Template",*z_arr],t_window=[-20,200])
 plt.show()
