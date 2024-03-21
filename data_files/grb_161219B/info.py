16f1503169603e5c3e70fa4e11d4978642a5f8a5
Michael Moss (NASA) <mjmoss@ndc.nasa.gov>
2024-03-21 15:31:57 -0400

    Merging grb_161219B files

---
+++ data_files/grb_161219B/info.py
@@ -0,0 +1,11 @@
+name = "161219B"
+z = 0.148 # Measured redshift 
+zmax = 1.5 # Highest redshift to simulate at
+fn = "data_files/grb_161219B/grb_161219B_1chan_64ms.lc" # file path to light curve
+t_true = 6.932 # true T90
+t_cut_min = -20 # cut data before this time 
+t_cut_max = 30 # cut data after this time
+# Best fit cut-off power law info:
+alpha = -1.294 # photon index
+ep = 61.877 # keV, peak energy 
+norm = 1.55e-01 # counts cm−2 s^−1 keV^−1, normalization
