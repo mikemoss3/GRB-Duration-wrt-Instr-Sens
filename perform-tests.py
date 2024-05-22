46db5cc82045576d7a0c6ac0ae409b6accc515fa
Mike Moss <mikejmoss3@gmail.com>
2024-05-22 09:41:42 -0400

    Version control for sims before background variation update

--- perform-tests.py
+++ perform-tests.py
@@ -30,7 +30,7 @@ grbs_names = np.array([
 	"081007",
 	"090424",
 	"091018",
-	# "091127",
+	"091127",
 	"100621A",
 	"100625A",
 	"100816A",
@@ -52,30 +52,16 @@ grbs_names = np.array([
 
 
 
-std1 = np.zeros(shape=0,dtype=[("name","U10"),("std",float)])
-std64 = np.zeros(shape=0,dtype=[("name","U10"),("std",float)])
 
 for i in range(len(grbs_names)):
 	grbp = importlib.import_module("data_files.grb_{}.info".format(grbs_names[i]), package=None) # Load GRB parameters
 	
 	template_grb = GRB(grbname = grbp.name, z=grbp.z)
-	template_grb.load_light_curve(grbp.fn, rm_trigtime=True, det_area=0.16)
-	template_grb.cut_light_curve(tmin=-20, tmax=20)
 	
-	std = np.array( [(grbp.name, np.mean(template_grb.light_curve['UNC']) )] , dtype=[("name","U10"),("std",float)] )
-	if template_grb.dt == 1:
-		std1 = np.append(std1, std)
+	sim_results = np.load("data_files/grb_{}/grb_{}_redshift_sim-results.tmp.txt.npy".format(grbp.name, grbp.name, grbp.name))
 	if template_grb.dt < 1:
-		std64 = np.append(std64, std)
-
-print("For 1s ave = ", np.mean(std1['std']))
-for i in range(len(std1)):
-	print("\t{}, {:.2f}".format(std1['name'][i], std1['std'][i]))
-print("For 64ms ave = ", np.mean(std64['std']))
-for i in range(len(std64)):
-	print("\t{}, {:.2f}".format(std64['name'][i], std64['std'][i]))
-
-
-bins = np.linspace(start=np.min(std1['std']), stop=np.max(std1['std']), num=len(std1))
-plt.hist(std1['std'],bins)
-plt.show()
+		sim_results['FLUENCE'] *= 0.064
+		sim_results['T100FLUENCE'] *= 0.064
+		sim_results['1sPeakFlux'] *= 0.064
+	np.save("data_files/grb_{}/grb_{}_redshift_sim-results.tmp.txt".format(grbp.name, grbp.name), sim_results)
+	
